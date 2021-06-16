8f22e7166dd2e947228f088b630d257fb152017c
Brendan DeForest <brendandeforest@icloud.com>
2021-06-16 12:23:55 -0400

    Fixed calculator.  Just need to correct some rounding errors next.

--- index.js
+++ index.js
@@ -18,24 +18,36 @@ function attachCalc() {
 
   //read afeet
   let a_ft = parseInt(document.getElementById('a_ft').value);
+  console.log(a_ft);
   //read ainches
   let a_in = parseInt(document.getElementById('a_in').value);
+  console.log(a_in);
   
   let a = a_ft + toFeet(a_in);
+  console.log(a);
 
   //read bfeet
   let b_ft = parseInt(document.getElementById('b_ft').value);
+  console.log(b_ft);
   //read binches
   let b_in = parseInt(document.getElementById('b_in').value);
+  console.log(b_in);
 
   let b = b_ft + toFeet(b_in);
+  console.log(b);
   //convert ainches, b inches to feet
   //calculate difference
   console.log("attachCalc() has completed");
   let result = (a-b);
-  resultFeet = toFeet(result);
-  resultInches = toInches(result);
-  document.getElementById("calcResults").innerHTML = '<strong>Result</strong> ' + resultFeet + "ft " + resultInches + " in";
+  let resultFeet = Math.floor(result);
+  let resultInches = getDecimal(result);
+  resultInches = Math.ceil(toInches(resultInches));
+
+function getDecimal(n) {
+	return (n - Math.floor(n));
+}
+
+  document.getElementById("calcResults").innerHTML = '<strong>Result</strong> ' + resultFeet + " ft, " + resultInches + " in";
 }
 
 //Attachment Calculator Button
